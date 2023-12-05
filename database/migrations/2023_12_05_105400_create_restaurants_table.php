ec96996f0480c40407e23bc4817de0376469a4e2
raimeiga <yusuke612@hotmail.com>
2023-12-05 11:05:08 +0900

    店（restaurants)テーブルのマイグレーションファイルの編集

--- database/migrations/2023_12_05_105400_create_restaurants_table.php
+++ database/migrations/2023_12_05_105400_create_restaurants_table.php
@@ -15,7 +15,14 @@ return new class extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name'); //店名
+            $table->text('description'); //説明
+            $table->integer('price')->unsigned(); //予算「～」がつくが、integerでいいのか？ unsignedを書くことで、マイナスの値が保存できないようにする
+            $table->time('hours');  // 予約時が営業時間内か否かを判断する設定にするなら、inetgerでなく、timeのほうがよいらしい。
+            $table->text('address');  //住所
+            $table->string('phone'); //電話番号
+            $table->string('holiday'); //定休日
+            $table->timestamps();  //作成日時・更新日時
         });
     }
 
